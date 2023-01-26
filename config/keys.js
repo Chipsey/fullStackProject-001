cae82efba0b7a15fe40a75b334bbf9f4f43b1d05
Udana D. Kodikara <chipseyofficial@gmail.com>
2023-01-26 23:53:51 +0530

    First commit

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Udana:12345@cluster0.e9uii4g.mongodb.net/test'
+    mongoURI: 'mongodb+srv://Udana:12345@cluster0.e9uii4g.mongodb.net/test',
+    secretOrKey: 'secret'
 };
--- server.js
+++ server.js
@@ -1,21 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 
-const users = require('./routes/api/users')
-const profile = require('./routes/api/profile')
-const posts = require('./routes/api/posts')
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 //DB Config
 const db = require('./config/keys').mongoURI;
-app.get('/', (req, res) => res.send('Hello world!'));
 
 //Connect to mongoDB
 mongoose
     .connect(db)
     .then(() => console.log('MongoDB Connected!!'))
     .catch(err => console.log('MongoDB not Connected!!'));
+
+
+//Passport middleware
+app.use(passport.initialize());
+
+//passport config
+require('./config/passport')(passport);
 
 //Use routes
 app.use('/api/users', users);
