61b3d4efd759fc13004cca94b7f9dbcdd23e1d6b
Udana D. Kodikara <chipseyofficial@gmail.com>
2023-02-10 04:49:23 +0530

    Frontend Getstarted and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,40 @@
-import react, { component } from 'react';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My react app</h1>
-    </div>
-  );
+import './style.css';
+
+import Navbar from './components/layout/Navbar';
+import Content from './components/layout/Content';
+import Login from './components/auth/Login';
+import Registration from './components/auth/Registration';
+
+
+let date = new Date()
+const hours = date.getHours()
+let mode
+if (hours >= 6 && hours <= 20) {
+    mode = "light"
+}
+else {
+    mode = "light"
+}
+
+
+class App extends Component {
+  render(){
+      return (
+        <Router>
+          <div>
+            <Navbar mode = {mode}/>
+              <Routes>
+                  <Route exact path='/' element= {<Content mode = {mode}/>}></Route>
+                  <Route exact path='/login' element= {<Login mode = {mode} />}></Route>
+                  <Route exact path='/registration' element= {<Registration />}></Route>
+              </Routes>
+          </div>
+        </Router>
+      );
+  }
 }
 
 export default App;
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <div className={this.props.mode}>
+        <nav>
+            <img src="logo.png" alt="logo" className="nav--logo"></img>
+            <ul className="nav--list">
+                <li></li>
+                <li>
+                  <Link className='nav-link' to="/">
+                    GET STARTED
+                  </Link>
+                </li>
+                <li>
+                  <Link className='nav-link' to="/login">
+                    LOGIN
+                  </Link>
+                </li>
+                <li>ABOUT</li>
+            </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar;
